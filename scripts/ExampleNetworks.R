0840f486115015bcbf96afe2c933021f2464f5bf
Chris Tokita <christopher.tokita@gmail.com>
2017-11-15 14:37:52 -0500

    update

---
+++ scripts/ExampleNetworks.R
@@ -0,0 +1,26 @@
+################################################################################
+#
+# Examine Network from last run
+#
+################################################################################
+
+g <- g_tot / gens
+
+# Drop edges below baseline interaction probability
+g[g < p] <- 0
+
+# Edgelist
+graph <- graph.adjacency(g, weighted = T)
+edgelist <- get.edgelist(graph)
+edgelist <- as.data.frame(edgelist)
+names(edgelist) <- c("Source", "Target")
+edgelist$Weight <- E(graph)$weight 
+
+# Nodelist
+nodelist <- data.frame(Id = rownames(threshMat))
+nodelist$Thresh1 <- threshMat[ , 1]
+nodelist$Thresh2 <- threshMat[ , 2]
+nodelist$ThreshRatio <- log(threshMat[ , 1] / threshMat[ , 2])
+
+write.csv(edgelist, file = "output/Networks/GroupSize70edgelistFIXED.csv", row.names = FALSE)
+write.csv(nodelist, file = "output/Networks/GroupSize70nodelistFIXED.csv", row.names = FALSE)
