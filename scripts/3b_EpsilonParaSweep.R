60c6d5578784cd790e422377ce18461e4ebce8bb
Chris Tokita <christopher.tokita@gmail.com>
2018-06-22 16:52:43 -0500

    Update parallel scripts

--- scripts/3b_EpsilonParaSweep.R
+++ scripts/3b_EpsilonParaSweep.R
@@ -18,7 +18,7 @@ library(snowfall)
 ####################
 # Initial paramters: Free to change
 # Base parameters
-Ns             <- seq(2, 100, 2) #vector of number of individuals to simulate
+Ns             <- seq(5, 100, 5) #vector of number of individuals to simulate
 m              <- 2 #number of tasks
 gens           <- 50000 #number of generations to run simulation 
 reps           <- 100 #number of replications per simulation (for ensemble)
@@ -33,7 +33,7 @@ quitP          <- 0.2 #probability of quitting task once active
 
 # Social Network Parameters
 p              <- 0.5 #baseline probablity of initiating an interaction per time step
-epsilons       <- seq(0, 0.5, 0.01) #relative weighting of social interactions for adjusting thresholds
+epsilons       <- seq(0, 0.25, 0.01) #relative weighting of social interactions for adjusting thresholds
 beta           <- 1.1 #probability of interacting with individual in same state relative to others
 
 
