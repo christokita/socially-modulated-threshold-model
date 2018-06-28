62878c854b040e99239e7f1d396225c9086b6b89
Chris Tokita <christopher.tokita@gmail.com>
2018-06-28 11:05:04 -0400

    Processing data

--- scripts/2b_Process_ClusterSimData.R
+++ scripts/2b_Process_ClusterSimData.R
@@ -10,7 +10,7 @@ library(RColorBrewer)
 library(scales)
 
 
-directory_path <- "output/Rdata/Sigma0-Epsilon0.1-Beta1.01/"
+directory_path <- "output/Rdata/Sigma0-Epsilon0.01-Beta1.1/"
 output_path <- "output/Rdata/_ProcessedData/"
 run_info <- gsub("^.*(Sigma.*)/$", "\\1", directory_path, perl = TRUE)
 
