2864c08b0959f2c191b1bd9686eaa45153c703ad
Chris Tokita <christopher.tokita@gmail.com>
2018-07-04 09:59:37 -0400

    plots

--- scripts/Analyze_Entropy.R
+++ scripts/Analyze_Entropy.R
@@ -80,5 +80,37 @@ gg_entropy
 ggsave(gg_entropy, file = "output/SpecializationPlots/Sigma0-Epsilon0.1-BetaSweep.png", 
        height = 45, width = 45, units = "mm", dpi = 800)
 
+####################
+# Only beta = 1.1
+####################
+select_entropy <- entropy %>%
+  filter(Model == "Social_Beta1.1")
+
+gg_solo <- ggplot(data = select_entropy, aes(x = n, colour = Model)) +
+  geom_line(aes(y = Mean),
+            size = 0.4) +
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
+                width = 0) +
+  geom_point(aes(y = Mean),
+             size = 1) +
+  theme_classic() +
+  ylab("Division of Labor") +
+  scale_x_continuous(breaks = seq(0, 100, 20)) +
+  scale_color_manual(values = "#636363", 
+                     name = expression("Interaction bias"(Beta))) +
+  theme(axis.text = element_text(colour = "black", size = 6),
+        axis.title = element_text(size = 7),
+        legend.position = "none",
+        legend.title = element_text(size = 7, 
+                                    face = "bold"),
+        legend.text = element_text(size = 6),
+        legend.key.height = unit(4, "mm"),
+        legend.key.width = unit(5, "mm"),
+        axis.ticks = element_line(size = 0.3),
+        axis.line = element_line(size = 0.3),
+        aspect.ratio = 1)
+gg_solo
+
+ggsave(gg_solo, filename = "output/SpecializationPlots/Beta1.1.svg", width = 60, height = 60, units = "mm")
 
 ############### Sweep across epsilon values ###############
