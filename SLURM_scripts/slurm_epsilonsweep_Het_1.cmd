1b15a9a237349a31a9f22e8931ddb3a21511bfb5
Chris Tokita <christopher.tokita@gmail.com>
2018-09-04 14:58:12 -0400

    More parameter sweep scripts

---
+++ SLURM_scripts/slurm_epsilonsweep_Het_1.cmd
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 20
+#SBATCH -t 47:59:00
+#SBATCH --mem-per-cpu=6G
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=end
+#SBATCH --mail-user=ctokita@princeton.edu
+
+cd
+Rscript scripts/3_para_sweep/3b_EpsilonParaSweep_Het_1.R
