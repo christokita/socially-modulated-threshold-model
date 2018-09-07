83ace11f58fe2618e7a53450c70b576131b4d336
Chris Tokita <christopher.tokita@gmail.com>
2018-09-07 15:38:32 -0400

    Analyzed fixed thresholds

---
+++ scripts/Analyze_Entropy_FixedThresh.R
@@ -0,0 +1,190 @@
+################################################################################
+#
+# Comparing various specialization plots
+#
+################################################################################
+
+rm(list = ls())
+source("scripts/util/__Util__MASTER.R")
+library(RColorBrewer)
+library(scales)
+
+
+############### Sweep across beta values ###############
+
+####################
+# Load data
+####################
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.1-Beta1.01.Rdata")
+compiled_data$Model <- "Social_Beta1.01"
+entropy_data <- compiled_data
+rm(compiled_data)
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.1-Beta1.05.Rdata")
+compiled_data$Model <- "Social_Beta1.05"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.1-Beta1.1.Rdata")
+compiled_data$Model <- "Social_Beta1.1"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.1-Beta1.2.Rdata")
+compiled_data$Model <- "Social_Beta1.2"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0.1-Epsilon0-Beta1.1-Delta0.6.Rdata")
+compiled_data$Model <- "Fixed"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+####################
+# Summarise data
+####################
+# Calculate mean and SE
+entropy <- entropy_data %>% 
+  group_by(Model, n) %>% 
+  summarise(Mean = mean(Dind),
+            SE = sd(Dind) / sqrt(length(Dind)))
+
+
+####################
+# Plot
+####################
+pal <- c("black", brewer.pal(5, "Greens")[2:5])
+
+gg_entropy <- ggplot(data = entropy_data, aes(x = n, colour = Model)) +
+  geom_point(aes(y = Dind),
+             size = 0.8,
+             alpha = 0.2,
+             stroke = 0) +
+  theme_classic() +
+  ylab("Division of labor") +
+  scale_x_continuous(breaks = seq(0, 100, 20)) +
+  scale_colour_manual(values = pal, 
+                     labels = c("1.2", "1.1"),
+                     name = expression("Interaction bias"(Beta))) +
+  theme(axis.text = element_text(colour = "black", size = 6),
+        axis.title = element_text(size = 7, face = "italic"),
+        legend.position = "none",
+        legend.title = element_text(size = 7, 
+                                    face = "bold"),
+        legend.text = element_text(size = 6),
+        legend.key.height = unit(4, "mm"),
+        legend.key.width = unit(5, "mm"),
+        axis.ticks = element_line(size = 0.3, color = "black"),
+        axis.line = element_line(size = 0.3, color = "black"),
+        aspect.ratio = 1) +
+  facet_wrap(~Model)
+gg_entropy
+
+# ggsave(gg_entropy, file = "output/SpecializationPlots/Sigma0-Epsilon0.1-BetaSweep.png", 
+#        height = 45, width = 45, units = "mm", dpi = 800)
+# ggsave(gg_entropy, file = "output/SpecializationPlots/Sigma0-Epsilon0.1-BetaSweep.svg", 
+#        height = 47, width = 47, units = "mm")
+
+####################
+# Only beta = 1.1
+####################
+select_entropy <- entropy %>%
+  filter(Model == "Social_Beta1.1")
+
+gg_solo <- ggplot(data = select_entropy, aes(x = n, colour = Model)) +
+  geom_line(aes(y = Mean),
+            size = 0.4) +
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
+                width = 0) +
+  geom_point(aes(y = Mean),
+             size = 1) +
+  theme_classic() +
+  ylab("Division of labor") +
+  scale_x_continuous(breaks = seq(0, 100, 20)) +
+  scale_color_manual(values = "#636363", 
+                     name = expression("Interaction bias"(Beta))) +
+  theme(axis.text = element_text(colour = "black", size = 6),
+        axis.title = element_text(size = 7, face = "italic"),
+        legend.position = "none",
+        legend.title = element_text(size = 7, 
+                                    face = "bold"),
+        legend.text = element_text(size = 6),
+        legend.key.height = unit(4, "mm"),
+        legend.key.width = unit(5, "mm"),
+        axis.ticks = element_line(size = 0.3),
+        axis.line = element_line(size = 0.3),
+        aspect.ratio = 1)
+gg_solo
+
+ggsave(gg_solo, filename = "output/SpecializationPlots/Beta1.1.svg", width = 45, height = 45, units = "mm")
+ggsave(gg_solo, filename = "output/SpecializationPlots/Beta1.1.png", width = 45, height = 45, units = "mm", dpi = 400)
+
+############### Sweep across epsilon values ###############
+rm(list = ls())
+source("scripts/util/__Util__MASTER.R")
+library(RColorBrewer)
+library(scales)
+
+####################
+# Load data
+####################
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0-Beta1.1.Rdata")
+compiled_data$Model <- "Social_Epsilon 0.0"
+entropy_data <- compiled_data
+rm(compiled_data)
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.1-Beta1.1.Rdata")
+compiled_data$Model <- "Social_Epsilon0.1"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+load("output/Rdata/_ProcessedData/Entropy/Sigma0-Epsilon0.4-Beta1.1.Rdata")
+compiled_data$Model <- "Social_Epsilon0.4"
+entropy_data <- rbind(entropy_data, compiled_data)
+rm(compiled_data)
+
+####################
+# Summarise data
+####################
+# Calculate mean and SE
+entropy <- entropy_data %>% 
+  group_by(Model, n) %>% 
+  summarise(Mean = mean(Dind),
+            SE = sd(Dind) / sqrt(length(Dind)))
+
+####################
+# Plot
+####################
+pal <- brewer.pal(5, "Greens")[c(2, 4, 5)]
+
+gg_entropy <- ggplot(data = entropy, aes(x = n, colour = Model)) +
+  geom_line(aes(y = Mean),
+            size = 0.4) +
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE),
+                width = 0) +
+  geom_point(aes(y = Mean),
+             size = 0.8) +
+  theme_classic() +
+  ylab("Division of labor") +
+  scale_x_continuous(breaks = seq(0, 100, 20)) +
+  scale_color_manual(values = pal, 
+                     labels = c("1.2", "1.1"),
+                     name = expression("Interaction bias"(Beta))) +
+  theme(axis.text = element_text(colour = "black", size = 6),
+        axis.title = element_text(size = 7, face = "italic"),
+        legend.position = "none",
+        legend.title = element_text(size = 7, 
+                                    face = "bold"),
+        legend.text = element_text(size = 6),
+        legend.key.height = unit(4, "mm"),
+        legend.key.width = unit(5, "mm"),
+        axis.ticks = element_line(size = 0.3, color = "black"),
+        axis.line = element_line(size = 0.3, color = "black"),
+        aspect.ratio = 1)
+gg_entropy
+
+ggsave(gg_entropy, file = "output/SpecializationPlots/Sigma0-Beta1.1-EpsSweep.png", 
+       height = 45, width = 45, units = "mm", dpi = 800)
+ggsave(gg_entropy, file = "output/SpecializationPlots/Sigma0-Beta1.1-EpsSweep.svg", 
+       height = 47, width = 47, units = "mm")
