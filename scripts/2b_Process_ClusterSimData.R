68b712dfb20b6d164e2ff97097011c451b60e0d8
Chris Tokita <christopher.tokita@gmail.com>
2018-09-21 15:25:00 -0500

    Rank corr code

--- scripts/2b_Process_ClusterSimData.R
+++ scripts/2b_Process_ClusterSimData.R
@@ -98,3 +98,22 @@ for (folder in list_folders) {
     }
   }
 }
+
+
+####################
+# Load rank corr data and bind, save
+####################
+files <- list.files(paste0(directory_path, "RankCorr"), full.names = TRUE)
+for (file in files) {
+  load(file)
+  if (!exists("compiled_data")) {
+    compiled_data <- data
+  } else {
+    compiled_data <- rbind(compiled_data, data)
+  }
+}
+compiled_data <- as.data.frame(compiled_data)
+save(compiled_data, file = paste0(output_path, "RankCorr/", run_info, ".Rdata"))
+rm(compiled_data)
+
+
---
+++ scripts/Analyze_SpecializationEntropy.R
@@ -0,0 +1,54 @@
+################################################################################
+#
+# Comparing various specialization plots
+#
+################################################################################
+
+rm(list = ls())
+source("scripts/util/__Util__MASTER.R")
+library(RColorBrewer)
+library(scales)
+
+
+####################
+# Load data
+####################
+load("output/Rdata/_ProcessedData/RankCorr/Sigma0-Epsilon0-Beta1.1_RankCorr.Rdata")
+
+####################
+# Process data
+####################
+rank_corr <- compiled_data %>% 
+  mutate(Spec = (Task1 + Task2) / 2) %>% 
+  group_by(n) %>% 
+  summarise(MeanSpec = mean(Spec),
+            SESpec = sd(Spec)/sqrt(length(Spec)))
+
+
+####################
+# Plot
+####################
+gg_spec <- ggplot(data = rank_corr, aes(x = n)) +
+  geom_line(aes(y = MeanSpec),
+            size = 0.4) +
+  geom_errorbar(aes(ymin = MeanSpec - SESpec, ymax = MeanSpec + SESpec),
+                width = 0) +
+  geom_point(aes(y = MeanSpec),
+             size = 0.8) +
+  theme_classic() +
+  xlab(expression(paste("Group Size (", italic(n), ")"))) +
+  ylab(expression(paste("Specialization (rank corr.)"))) +
+  scale_x_continuous(breaks = seq(0, 100, 20)) +
+  scale_y_continuous(limits = c(0, 1)) +
+  theme(axis.text = element_text(colour = "black", size = 6),
+        axis.title = element_text(size = 7, face = "italic"),
+        legend.position = "none",
+        legend.title = element_text(size = 7, 
+                                    face = "bold"),
+        legend.text = element_text(size = 6),
+        legend.key.height = unit(4, "mm"),
+        legend.key.width = unit(5, "mm"),
+        axis.ticks = element_line(size = 0.3, color = "black"),
+        axis.line = element_line(size = 0.3, color = "black"),
+        aspect.ratio = 1)
+gg_spec
